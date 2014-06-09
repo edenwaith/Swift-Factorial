4a2399f351be4f036bc19581e87067adeed4397c
Chad Armstrong <ChadArmstrong@Chads-MacBook-Pro-2.local>
2014-06-09 11:40:59 -0500

    Initial Commit

---
+++ Swift Factorial/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift Factorial
+//
+//  Created by Chad Armstrong on 6/9/14.
+//  Copyright (c) 2014 Edenwaith. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+	@IBOutlet var window: NSWindow
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification?) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Swift Factorial/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swift Factorial
+//
+//  Created by Chad Armstrong on 6/9/14.
+//  Copyright (c) 2014 Edenwaith. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
