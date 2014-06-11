c8ab63045caa02cb621d41167161260f53275c26
Chad Armstrong <ChadArmstrong@Chads-MacBook-Pro-2.local>
2014-06-10 20:32:33 -0500

    Initial commit for Swift Factorial

--- Swift Factorial/AppDelegate.swift
+++ Swift Factorial/AppDelegate.swift
@@ -11,16 +11,24 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
 	@IBOutlet var window: NSWindow
+	@IBOutlet var textField: NSTextField
+	@IBOutlet var button: NSButton
 
 
 	func applicationDidFinishLaunching(aNotification: NSNotification?) {
 		// Insert code here to initialize your application
+		self.window.center();
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification?) {
 		// Insert code here to tear down your application
 	}
 
+	@IBAction func calculateButtonClicked(sender : AnyObject) {
+		var originalValue: Double = self.textField.doubleValue;
+		var newValue: Double = exp(lgamma(originalValue + 1));
+		self.textField.doubleValue = newValue;
+	}
 
 }
 
